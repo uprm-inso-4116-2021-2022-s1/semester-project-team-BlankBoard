a81291b2d8e76fb4ee1732d135b6ba6350cccfdf
Natalia Lopez <natalia.lopez17@upr.edu>
2021-09-25 19:41:14 -0400

    created the users and drawings tables

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,16 @@
-CREATE TABLE B_USER
-(ID NUMERIC,
-    USERNAME VARCHAR(255));
+/*
+* where to store table creations and modifications
+*/
+create table Users(uid Integer, username varchar(255),
+password varchar(255), email varchar(255), followers Integer);
+
+insert into users(uid, username, password, email, followers)
+values(1,'jackskellington@7','1234','jackskell@gmail.com',100000);
+
+/****************************************************************************/
+
+create table drawings(did Integer, likes Integer, 
+replies Integer, drawingURL varchar(255));
+
+insert into drawings(did, likes, replies, drawingurl)
+values(1,50,100,'www.clickbait.com');
