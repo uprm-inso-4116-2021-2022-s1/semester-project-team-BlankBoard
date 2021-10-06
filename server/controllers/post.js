7182e21bcc34181b6a552d2d6fcaed886822071d
jose.bermudez8 <jose.bermudez8@upr.edu>
2021-10-05 21:40:45 -0400

    added server routes

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,12 +1,40 @@
 
-const posts = async (req, res, pool) => {
+const post = async (req, res, pool) => {
     try {
-        res.status(200).send('This is the posts get request.')
+        res.status(200).send('This is the post post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const replyID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the replyID post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const getPosts = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the getPosts get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const getPostsByID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the getPostsByID get request.')
     } catch (e) {
         res.status(400).send(err);
     }
 }
 
 module.exports = {
-    posts
+    post,
+    replyID,
+    getPosts,
+    getPostsByID
+
 }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,6 +7,88 @@ const users = async (req, res, pool) => {
     }
 }
 
+const userID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the userByID get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const followID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the followID post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const followedByID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the followedByID get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const followsID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the followsID get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const unfollowID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the unfollowID post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const blockID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the blockID post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const blockedByID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the blockedByID get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const blockingID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the blockingID get request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
+const unblockID = async (req, res, pool) => {
+    try {
+        res.status(200).send('This is the unblockID post request.')
+    } catch (e) {
+        res.status(400).send(err);
+    }
+}
+
 module.exports = {
-    users
+    users,
+    userID,
+    followID,
+    followedByID,
+    followsID,
+    unfollowID,
+    blockID,
+    blockedByID,
+    blockingID,
+    unblockID
+    
 }
