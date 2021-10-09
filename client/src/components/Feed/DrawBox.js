f25bab2bfdcd262d1059dd8eea50472282d65f5c
Anthony Mendez Frias <anthonyl.mendezf@gmail.com>
2021-10-09 17:15:47 -0400

    changed the text of a box to specify it is a placeholder

--- client/src/components/Feed/DrawBox.js
+++ client/src/components/Feed/DrawBox.js
@@ -8,13 +8,8 @@ function DrawBox() {
         <form>
             <div className="drawBox__input"> 
                 <Avatar src="https://media.wired.co.uk/photos/606d9c691e0ddb19555fb809/4:3/w_2664,h_1998,c_limit/dog-unsolicited.jpg"></Avatar>
-                <input placeholder="Are you ready to draw? :D"/>
+                <input placeholder="Drawing 'box' Location Placeholder"/>
             </div>
-                <input 
-                className="drawBox__imageInput"
-                placeholder="Enter image URL"
-                type="text"
-                />
             <Button className="drawBox__drawButton">Draw</Button>
         </form>
     </div>
