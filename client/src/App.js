ba612d35bebf246262afced7c8970d05ace2190b
Kurcell <kevnpurcell@gmail.com>
2021-10-31 23:19:57 -0400

    added Settings route to client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import Register from './views/Register';
+import Settings from './views/Settings';
 import Error404 from './views/Error404';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/settings" component={Settings} />
           <Route path="*" component={Error404} />
         </Switch>
       </Router>
