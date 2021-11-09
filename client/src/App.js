39bda0acbc73a901d5d2d9c5205058472893eb86
carlos <carlos.torres62@upr.edu>
2021-11-08 20:37:58 -0400

    added user posts list (fix variables)

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import Register from './views/Register';
+import Profile from './views/Profile';
 import Error404 from './views/Error404';
 import "./App.css";
-import Canvas from './components/Canvas/Canvas';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/canvas" component={Canvas} />
+          <Route exact path="/profile" component={Profile} />
           <Route path="*" component={Error404} />
         </Switch>
       </Router>
--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button } from "@mui/material";
+import { ImageList, ImageListItem, Modal } from "@mui/material";
 import Canvas from "../../components/Canvas/Canvas";
 // import { withRouter } from "react-router-dom";
 import "./Profile.css";
@@ -35,13 +35,33 @@ function Profile() {
   const numOfPosts = 0;
   const numOfComments = 0;
 
+  const posts = [
+    'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    'https://images.unsplash.com/photo-1589118949245-7d38baf380d6'
+  ];
+
+  // const posts = [];
+
   const showPosts = () => {
-    let i = 0;
-    for(i = 0; i < 12; i++) {
-      return(
-        <div></div>
-      );
-    }
+    return (
+        <ImageList cols={3} gap={32}>
+          {posts.map((post) => (
+            <ImageListItem>
+              <img alt="" src={`${post}?w=320&h=320&fit=crop&auto=format`} loading="lazy" />
+            </ImageListItem>
+          ))}
+        </ImageList>
+    );
   }
 
   // render() {
@@ -50,9 +70,7 @@ function Profile() {
         <div className="userInfo">
           <div className="pfp" onClick={openModal}></div>
           <Modal className="modalWindow" open={showModal} onClose={closeModal}>{Canvas("pfp", showModal)}</Modal>
-          <div className="intro">
             <div className="username">{username}</div>
-          </div>
           <div className="stats">
             <div className="number">{numOfPosts}</div>
             <div className="number">{numOfComments}</div>
@@ -60,7 +78,7 @@ function Profile() {
             <div className="text">comments</div>
           </div>
         </div>
-        <div className="posts">POSTS{showPosts()}</div>
+        <div className="posts">{showPosts()}</div>
       </div>
     );
   // }
