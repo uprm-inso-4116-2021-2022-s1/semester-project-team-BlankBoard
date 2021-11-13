f89089271ed464c0e1020131046ce962c1c6a648
carlos <carlos.torres62@upr.edu>
2021-11-12 22:33:40 -0400

    change app js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import Register from './views/Register';
-import Profile from './views/Profile';
 import Error404 from './views/Error404';
 import "./App.css";
 
@@ -14,7 +13,6 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/profile" component={Profile} />
           <Route path="*" component={Error404} />
         </Switch>
       </Router>
